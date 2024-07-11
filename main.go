4cb95e1dd209b061a2c41a0607beae3a067b5b61
Buddha <buddha@shupl.icu>
2024-07-11 22:38:32 +0800

    your code is good but it's mine now

--- main.go
+++ main.go
@@ -302,8 +302,8 @@ func release(source string, destination string, output string, cnOutput string, 
 
 func main() {
 	err := release(
-		"v2fly/domain-list-community",
-		"sagernet/sing-geosite",
+		"uptonedward/domain-list-community",
+		"uptonedward/sing-geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
